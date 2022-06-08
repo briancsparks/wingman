4175a633e89b0192a22679bae96bb8b3edcb7398
Brian C Sparks <briancsparks@gmail.com>
2022-06-08 13:51:32 -0700

    WIP

--- cmd/root.go
+++ cmd/root.go
@@ -18,13 +18,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
   Use:   "wingman",
-  Short: "A brief description of your application",
-  Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+  Short: "Your personal developer-helper app",
+  Long: `
+Wingman watches your keystrokes and does helpful tricks. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Move windows to places.
+More good stuff here.`,
+
   // Uncomment the following line if your bare application
   // has an action associated with it:
   //	Run: func(cmd *cobra.Command, args []string) { },
