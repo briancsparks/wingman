e808d528f805f83bd0b157d927e4bdc20b913702
Brian C Sparks <briancsparks@gmail.com>
2022-06-14 08:13:44 -0700

    leave alt-tab alone

--- wingman/mainloop.go
+++ wingman/mainloop.go
@@ -67,10 +67,10 @@ func RunKbMove() error {
       fmt.Println("Shutdown")
       return nil
 
-    case k := <-keyboardChan:
-      //fmt.Printf("Keyb: %-12v vkcode: %-12v sccode: %4v t: %10v\n", k.Message, k.VKCode, k.ScanCode, k.Time)
-      fmt.Printf("%-12v %-12v (%v)\n", k.Message, k.VKCode, k.ScanCode)
-      continue
+      //case k := <-keyboardChan:
+      //  //fmt.Printf("Keyb: %-12v vkcode: %-12v sccode: %4v t: %10v\n", k.Message, k.VKCode, k.ScanCode, k.Time)
+      //  fmt.Printf("%-12v %-12v (%v)\n", k.Message, k.VKCode, k.ScanCode)
+      //  continue
 
     }
   }
@@ -142,14 +142,25 @@ func handler(c chan<- types.KeyboardEvent) types.HOOKPROC {
     // ----- Engaged
     if mode == engaged {
       callNext = false
-      func() {
-        _ = isDown(message)
-        _ = isUp(message)
-      }()
+
+      if vkcode == types.VK_LMENU || vkcode == types.VK_TAB {
+        callNext = true
+
+      } else {
+        // Use an immediately-called function to avoid the 'goto NEXT' jumping over declarations
+        func() {
+          //ctrl := isCtrlDown(vkcode, message)
+          _ = isCtrlDown(vkcode, message)
+          _ = isShiftDown(vkcode, message)
+          _ = isWinKeyDown(vkcode, message)
+          _ = isAltDown(vkcode, message)
+          _ = isMenuKeyDown(vkcode, message)
+        }()
+      }
     }
 
   NEXT:
-    fmt.Printf("%-12v %-12v (%v); wproc: %v, w: %v, t1: %v, t1: %v, m: %v\n", message, vkcode, scancode, code, wParam, trigger1, trigger2, mode)
+    fmt.Printf("%-13v %-13v (%v); wproc: %v, w: %v, t1: %v, t1: %v, m: %v\n", message, vkcode, scancode, code, wParam, trigger1, trigger2, mode)
 
     // Do not call this, if you want to eat the key, otherwise the active window will get it.
     if callNext {
@@ -168,6 +179,88 @@ func isDown(m types.Message) bool {
 func isUp(m types.Message) bool {
   return m == types.WM_KEYUP || m == types.WM_SYSKEYUP
 }
+
+func isCtrlDown(k types.VKCode, m types.Message) bool {
+  return isCtrl(k) && isDown(m)
+}
+
+func isShiftDown(k types.VKCode, m types.Message) bool {
+  return isShift(k) && isDown(m)
+}
+
+func isWinKeyDown(k types.VKCode, m types.Message) bool {
+  return isWinKey(k) && isDown(m)
+}
+
+func isAltDown(k types.VKCode, m types.Message) bool {
+  return isAlt(k) && isDown(m)
+}
+
+func isMenuKeyDown(k types.VKCode, m types.Message) bool {
+  return isMenuKey(k) && isDown(m)
+}
+
+func isCtrl(k types.VKCode) bool {
+  return k == types.VK_LCONTROL || k == types.VK_RCONTROL
+}
+
+func isShift(k types.VKCode) bool {
+  return k == types.VK_LSHIFT || k == types.VK_RSHIFT
+}
+
+func isWinKey(k types.VKCode) bool {
+  return k == types.VK_LWIN || k == types.VK_RWIN
+}
+
+func isAlt(k types.VKCode) bool {
+  return k == types.VK_LMENU || k == types.VK_RMENU
+}
+
+func isMenuKey(k types.VKCode) bool {
+  return k == types.VK_APPS
+}
+
+//VK_LMENU               VKCode = 0xA4 // Left MENU key
+//VK_RMENU               VKCode = 0xA5 // Right MENU key
+//VK_LWIN                VKCode = 0x5B // Left Windows key (Natural keyboard)
+//VK_RWIN                VKCode = 0x5C // Right Windows key (Natural keyboard)
+//VK_APPS                VKCode = 0x5D // Applications key (Natural keyboard)
+
+//WM_KEYDOWN    VK_LEFT       (75); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_LEFT       (75); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_UP         (72); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_UP         (72); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_RIGHT      (77); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_RIGHT      (77); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_DOWN       (80); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_DOWN       (80); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_LCONTROL   (29); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_C          (46); wproc: 0, w: 256, t1: false, t1: false, m: normal
+
+//WM_KEYDOWN    VK_F1         (59); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_F1         (59); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_F10        (68); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_F10        (68); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_F11        (87); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_F11        (87); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_F12        (88); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_F12        (88); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_MEDIA_PLAY_PAUSE (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_MEDIA_PLAY_PAUSE (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_MEDIA_PREV_TRACK (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_MEDIA_PREV_TRACK (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_MEDIA_NEXT_TRACK (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_MEDIA_NEXT_TRACK (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_VOLUME_MUTE (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_VOLUME_MUTE (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_VOLUME_DOWN (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_VOLUME_DOWN (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_VOLUME_UP  (0); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_VOLUME_UP  (0); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_CAPITAL    (58); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_CAPITAL    (58); wproc: 0, w: 257, t1: false, t1: false, m: normal
+//WM_KEYDOWN    VK_CAPITAL    (58); wproc: 0, w: 256, t1: false, t1: false, m: normal
+//WM_KEYUP      VK_CAPITAL    (58); wproc: 0, w: 257, t1: false, t1: false, m: normal
 
 // --------------------------------------------------------------------------------------------------------------------
 // See: https://github.com/moutend/go-hook/blob/develop/examples/swapkeys/main.go
