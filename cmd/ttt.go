c81c06f6dd4fc6d3da6fbb5c3f63c277d7e4be79
Brian C Sparks <briancsparks@gmail.com>
2022-06-29 00:02:01 -0700

    refactoring DoTttX

--- cmd/ttt.go
+++ cmd/ttt.go
@@ -23,43 +23,10 @@ Test what yer working on.`,
 }
 
 func DoTtt() {
-  fmt.Println("mvw called")
-  wingman.RunKbMove0()
-  fmt.Printf("Run done\n")
-}
-
-func DoTtt4() {
-  wingman.ThreeTwoOneGo()
-  _ = wingman.MoveActiveWindowDir(wingman.ScreenLeft, 20)
-}
-
-func DoTtt3() {
-  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
-  //wr := win.RECT{Left: 28, Top: 147, Right: 1156, Bottom: 1050} // Left
-  //hwnd, _ := wingman.FindWindowByClassName("Notepad")
-
-  rect := wingman.ImageRectangle(wr)
-  wingman.ThreeTwoOneGo()
-  _ = wingman.MoveActiveWindowTo(rect)
-}
-
-func DoTtt2() {
-  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
-  //wr := win.RECT{Left: 28, Top: 147, Right: 1156, Bottom: 1050} // Left
-  //hwnd, _ := wingman.FindWindowByClassName("Notepad")
-
-  rect := wingman.ImageRectangle(wr)
-  wingman.ThreeTwoOneGo()
-  _ = wingman.MoveActiveWindowTo(rect)
-}
-
-func DoTtt1() {
-  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
-  //wr := win.RECT{Left: 28, Top: 147, Right: 1156, Bottom: 1050} // Left
-  hwnd, _ := wingman.FindWindowByClassName("Notepad")
-
-  rect := wingman.ImageRectangle(wr)
-  _ = wingman.MoveWindowTo(hwnd, rect)
+  DoTtt5()
+  //fmt.Println("mvw called")
+  //wingman.RunKbMove0()
+  //fmt.Printf("Run done\n")
 }
 
 func init() {
@@ -74,4 +41,109 @@ func init() {
   // Cobra supports local flags which will only run when this command
   // is called directly, e.g.:
   // tttCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+  tttCmd.AddCommand(ttt1Cmd)
+  tttCmd.AddCommand(ttt2Cmd)
+  tttCmd.AddCommand(ttt3Cmd)
+  tttCmd.AddCommand(ttt4Cmd)
+  tttCmd.AddCommand(ttt5Cmd)
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ttt5Cmd represents the ttt5 command
+var ttt5Cmd = &cobra.Command{
+  Use:   "5",
+  Short: "wingman.RunKbMove0 [Run main loop]",
+
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("ttt5 called")
+    DoTtt5()
+  },
+}
+
+func DoTtt5() {
+  wingman.RunKbMove0()
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ttt1Cmd represents the ttt1 command
+var ttt1Cmd = &cobra.Command{
+  Use:   "1",
+  Short: "wingman.MoveWindowTo (notepad, main)",
+
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("ttt1 called")
+    DoTtt1()
+  },
+}
+
+func DoTtt1() {
+  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
+  hwnd, _ := wingman.FindWindowByClassName("Notepad")
+
+  rect := wingman.ImageRectangle(wr)
+  //fmt.Printf("ttt1: hwnd: %v, rect: %v\n", hwnd, rect)
+  _ = wingman.MoveWindowTo(hwnd, rect)
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ttt2Cmd represents the ttt2 command
+var ttt2Cmd = &cobra.Command{
+  Use:   "2",
+  Short: "wingman.MoveActiveWindowTo (main)",
+
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("ttt2 called")
+    DoTtt2()
+  },
+}
+
+func DoTtt2() {
+  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
+
+  rect := wingman.ImageRectangle(wr)
+  wingman.ThreeTwoOneGo()
+  _ = wingman.MoveActiveWindowTo(rect)
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ttt3Cmd represents the ttt3 command
+var ttt3Cmd = &cobra.Command{
+  Use:   "3",
+  Short: "wingman.MoveActiveWindowTo (main)",
+
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("ttt3 called")
+    DoTtt3()
+  },
+}
+
+func DoTtt3() {
+  wr := win.RECT{Left: 288, Top: 132, Right: 1828, Bottom: 1019} // Main
+
+  rect := wingman.ImageRectangle(wr)
+  wingman.ThreeTwoOneGo()
+  _ = wingman.MoveActiveWindowTo(rect)
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ttt4Cmd represents the ttt4 command
+var ttt4Cmd = &cobra.Command{
+  Use:   "4",
+  Short: "wingman.MoveActiveWindowDir (left)",
+
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("ttt4 called")
+    DoTtt4()
+  },
+}
+
+func DoTtt4() {
+  wingman.ThreeTwoOneGo()
+  _ = wingman.MoveActiveWindowDir(wingman.ScreenLeft, 20)
 }
